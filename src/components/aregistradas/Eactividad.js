84723bad82c01f523976cc81053ea5914bce5edb
SaulSandovalM <sauldevelop@gmail.com>
2020-08-27 13:58:24 -0500

    Alme

--- src/components/aregistradas/Eactividad.js
+++ src/components/aregistradas/Eactividad.js
@@ -95,26 +95,112 @@ export default class Aactividad extends Component {
   }
 
   render () {
+    const { convocados, convoca, fechai, fechaf, tipoA, estado, internaE,
+            municipio, quien, lugar, imparte, desc } = this.state
     return (
       <div>
         <div className='container-aactividad'>
           <div>
-            <h1>Seguimiento de Actividad</h1>
+            <h1>Editar de Actividad</h1>
           </div>
-          <div className='content-list-ea'>
-            <div className='title-t-es'>
-              <p className='style-p-ea'>Titulo</p>
+          <form className='content-aa' onSubmit={this.onSubmit}>
+            <div className='input-c-c'>
+              <p>Convodados:</p>
+              <input className='style-check' type='checkbox' name='convocados'
+                value={convocados} onChange={this.onChange}/>
+              <input className='style-check' type='checkbox' />
             </div>
-            <div className='title-t-es' style={{borderLeft: '1px solid'}}>
-              <p className='style-p-ea'>Descripcion</p>
+            <div className='content-row'>
+              <div className='input-c-c'>
+                <p className='p-t-aa'>Quien Convoca:</p>
+                <select className='select' name='convoca' value={convoca}
+                  onChange={this.onChange}>
+                  <option></option>
+                  <option value="grapefruit">Grapefruit</option>
+                  <option value="lime">Lime</option>
+                  <option selected value="coconut">Coconut</option>
+                  <option value="mango">Mango</option>
+                </select>
+              </div>
+              <div className='input-c-c'>
+                <p className='p-t-aa'>Fecha de Inicio:</p>
+                <input type='date' name='fechai' value={fechai}
+                  onChange={this.onChange} />
+              </div>
             </div>
-            <div className='title-t-es' style={{borderLeft: '1px solid'}}>
-              <p className='style-p-ea'>Evidencia</p>
+            <div className='content-row'>
+              <div className='input-c-c'>
+                <p className='p-t-aa'>Tipo de Actividad:</p>
+                <select className='select' name='tipoA' value={tipoA}
+                  onChange={this.onChange}>
+                  <option></option>
+                  <option value="grapefruit">Grapefruit</option>
+                  <option value="lime">Lime</option>
+                  <option selected value="coconut">Coconut</option>
+                  <option value="mango">Mango</option>
+                </select>
+              </div>
+              <div className='input-c-c'>
+                <p className='p-t-aa'>Fecha de Fin:</p>
+                <input type='date' name='fechaf' value={fechaf}
+                  onChange={this.onChange} />
+              </div>
             </div>
-            <div className='title-t-es' style={{borderLeft: '1px solid'}}>
-              <p className='style-p-ea'>Editar</p>
+            <div className='content-row'>
+              <div className='input-c-c'>
+                <p className='p-t-aa'>Estado:</p>
+                <select className='select' name='estado' value={estado}
+                  onChange={this.onChange}>
+                  <option></option>
+                  <option value="grapefruit">Grapefruit</option>
+                  <option value="lime">Lime</option>
+                  <option selected value="coconut">Coconut</option>
+                  <option value="mango">Mango</option>
+                </select>
+              </div>
+              <div className='input-c-c'>
+                <p className='p-t-aa'>Interna Externa:</p>
+                <input className='style-check-i' type='checkbox'
+                  name='internaE' value={internaE}onChange={this.onChange} />
+                <input className='style-check-i' type='checkbox' />
+              </div>
             </div>
-          </div>
+            <div className='content-row'>
+              <div className='input-c-c'>
+                <p className='p-t-aa'>Municipio:</p>
+                <select className='select' name='municipio' value={municipio}
+                  onChange={this.onChange}>
+                  <option></option>
+                  <option value="grapefruit">Grapefruit</option>
+                  <option value="lime">Lime</option>
+                  <option selected value="coconut">Coconut</option>
+                  <option value="mango">Mango</option>
+                </select>
+              </div>
+              <div className='input-c-c'>
+                <p className='p-t-aa'>Con quien:</p>
+                <input name='quien' value={quien} onChange={this.onChange} />
+              </div>
+            </div>
+            <div className='content-row'>
+              <div className='input-c-c'>
+                <p className='p-t-aa'>Lugar:</p>
+                <input name='lugar' value={lugar} onChange={this.onChange} />
+              </div>
+              <div className='input-c-c'>
+                <p className='p-t-aa'>Imparte:</p>
+                <input name='imparte' value={imparte} onChange={this.onChange} />
+              </div>
+            </div>
+            <div>
+              <p>Descripción</p>
+              <textarea cols='80' rows='3' name='desc' value={desc}
+                onChange={this.onChange} />
+            </div>
+            <div className='button-aa'>
+              <button className='style-button-aa' type='submit' >Guardar</button>
+            </div>
+          </form>
         </div>
       </div>
     )
