b414a0bd6d6f319b374c46b38538d79074a5a670
SaulSandovalM <sauldevelop@gmail.com>
2020-09-28 22:34:10 -0500

    icons

--- src/components/common/nav/Nav.js
+++ src/components/common/nav/Nav.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom'
 import { logoutUser } from '../../../actions'
 import { connect } from 'react-redux'
 import './Nav.css'
+import { MailOutline, Search, CalendarToday, ListAlt, EventAvailable, Person } from '@material-ui/icons';
 import logoH from '../../../imgs/logo_h.svg'
 
 class Nav extends Component {
@@ -22,27 +23,42 @@ class Nav extends Component {
         </div>
         <div className='navbar-left'>
           <Link to='/Listademensajes' className='deco'>
-            <h3 className='nav-t'>Lista de Mensajes</h3>
+            <h3 className='nav-t'>
+              <MailOutline className='icon-lm' />
+              Mensajes
+            </h3>
           </Link>
         </div>
         <div className='navbar-left'>
           <Link to='/ActividadesRegistradas' className='deco'>
-            <h3 className='nav-t'>Actividades Registradas</h3>
+            <h3 className='nav-t'>
+              <CalendarToday className='icon-lm' />
+              Actividades
+            </h3>
           </Link>
         </div>
         <div className='navbar-left'>
           <Link to='/BusquedaActividad' className='deco'>
-            <h3 className='nav-t'>Busqueda</h3>
+            <h3 className='nav-t'>
+              <Search className='icon-lm' />
+              Busqueda
+            </h3>
           </Link>
         </div>
         <div className='navbar-left'>
           <Link to='/Reportes' className='deco'>
-            <h3 className='nav-t'>Reportes</h3>
+            <h3 className='nav-t'>
+              <ListAlt className='icon-lm' />
+              Reportes
+            </h3>
           </Link>
         </div>
         <div className='navbar-left'>
           <Link to='/Autorizacion' className='deco'>
-            <h3 className='nav-t'>Autorizacion</h3>
+            <h3 className='nav-t'>
+              <EventAvailable className='icon-lm' />
+              Autorizacion
+            </h3>
           </Link>
         </div>
         <div className='navbar-left'>
@@ -52,6 +68,7 @@ class Nav extends Component {
               className='nav-t'
               style={{ background: '#092432', border: 'none' }}>
               <h3 className='nav-t'>
+                <Person className='icon-lm' />
                 Cerrar Sesion
               </h3>
             </button>
