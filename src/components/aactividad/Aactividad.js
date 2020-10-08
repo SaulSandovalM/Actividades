45e9b41514257390507868ed222a7f5d8bbc8ced
Alme95 <masc956@gmail.com>
2020-10-07 22:04:05 -0500

    Agregar_Actividad_071020

--- src/components/aactividad/Aactividad.js
+++ src/components/aactividad/Aactividad.js
@@ -4,6 +4,9 @@ import firebase from '../../Firebase'
 import TextField from '@material-ui/core/TextField'
 import Fab from '@material-ui/core/Fab'
 import DoneIcon from '@material-ui/icons/Done'
+import Select from '@material-ui/core/Select'
+import MenuItem from '@material-ui/core/MenuItem'
+import InputLabel from '@material-ui/core/InputLabel'
 
 export default class Aactividad extends Component {
   // constructor () {
@@ -187,71 +190,170 @@ export default class Aactividad extends Component {
     // var date = (meses[f.getMonth()] + ' ' + f.getDate() + ', ' + f.getFullYear())
     // this.state.fecha = date
 
-    return (
-      <div className='mg-conta'>
-        <div>
-          <div className='divtop-mg' />
-          <div className='form-content-gm'>
-            <form noValidate autoComplete='off' className='mensajesg-container'>
-              <h2>Agregar Actividad</h2>
-              <p className='martop-dt'>Fecha y hora de inicio *</p>
-              <div className='date-cont'>
+      return (
+        <div className='mg-conta'>
+          <div>
+            <div className='divtop-mg' />
+            <div className='form-content-gm'>
+              <form noValidate autoComplete='off' className='mensajesg-container2'>
+                <h2>Agregar Actividad</h2>
+                <p className='martop-dt'>Fecha y hora de inicio *</p>
+                <div className='date-cont'>
+                  <TextField
+                    type='date'
+                    style={{ width: '45%' }}
+                    name='fecha'
+                    required
+                  />
+                  <TextField
+                    type='time'
+                    style={{ width: '45%' }}
+                    name='fecha'
+                    required
+                  />
+                </div>
+                <p className='martop-dt'>Fecha y hora de Finalizacion *</p>
+                <div className='date-cont'>
+                  <TextField
+                    type='date'
+                    style={{ marginTop: '30px', width: '45%' }}
+                    name='fecha'
+                    placeholdercolor='grey'
+                    required
+                  />
+                  <TextField
+                    type='time'
+                    style={{ marginTop: '30px', width: '45%' }}
+                    name='fecha'
+                    required
+                  />
+                </div>
                 <TextField
-                  type='date'
-                  style={{ width: '45%' }}
-                  name='fecha'
+                  label='Actividad'
+                  style={{ marginTop: '15px' }}
+                  name='asunto'
+                  required
+                />
+                <InputLabel id="demo-simple-select-outlined-label">Tipo Actividad</InputLabel>
+                <Select
+                  labelId="demo-simple-select-outlined-label"
+                  id="demo-simple-select-outlined"
+                  label='Tipo Actividad'
+                  style={{ marginTop: '15px' }}
+                  name='descripcion'
+                  required
+                  >
+                  <MenuItem value="">
+                    <em> </em>
+                  </MenuItem>
+                  <MenuItem value={10}>Taller</MenuItem>
+                  <MenuItem value={20}>Reunión de Trabajo</MenuItem>
+                  <MenuItem value={30}>Curso</MenuItem>
+                  <MenuItem value={40}>Conferencia</MenuItem>
+                  <MenuItem value={50}>Otro</MenuItem>
+                </Select>
+                <TextField
+                  label='Actividad organizada por procuraduría'
+                  style={{ marginTop: '15px' }}
+                  name='organiza'
                   required
                 />
                 <TextField
-                  type='time'
-                  style={{ width: '45%' }}
-                  name='fecha'
-                  required
-                />
-              </div>
-              <p className='martop-dt'>Fecha y hora de Finalizacion *</p>
-              <div className='date-cont'>
-                <TextField
-                  type='date'
-                  style={{ marginTop: '30px', width: '45%' }}
-                  name='fecha'
-                  placeholdercolor='grey'
+                  label='Convoca'
+                  style={{ marginTop: '15px' }}
+                  name='convoca'
                   required
                 />
                 <TextField
-                  type='time'
-                  style={{ marginTop: '30px', width: '45%' }}
-                  name='fecha'
+                  label='Dependencia/Institución que convoca'
+                  style={{ marginTop: '15px' }}
+                  name='dependencia'
                   required
                 />
-              </div>
-              <TextField
-                label='Actividad'
-                style={{ marginTop: '15px' }}
-                name='asunto'
-                required
-              />
-              <TextField
-                label='Tipo Actividad'
-                style={{ marginTop: '15px' }}
-                name='descripcion'
-                required
-              />
-              <TextField
-                label='Convoca'
-                style={{ marginTop: '15px' }}
-                name='fecha'
-                required
-              />
-              <div className='add-gb'>
-                <Fab color='primary' aria-label='add' style={{ background: 'green' }} type='submit'>
-                  <DoneIcon />
-                </Fab>
-              </div>
-            </form>
+                <InputLabel id="demo-simple-select-outlined-label">Estados</InputLabel>
+                <Select
+                  labelId="demo-simple-select-outlined-label"
+                  id="demo-simple-select-outlined"
+                  label='Estados'
+                  style={{ marginTop: '15px' }}
+                  name='estados'
+                  required
+                  >
+                  <MenuItem value="">
+                    <em> </em>
+                  </MenuItem>
+                  <MenuItem value={10}>Aguascalientes</MenuItem>
+                  <MenuItem value={20}>Hidalgo</MenuItem>
+                  <MenuItem value={30}>Nuevo León</MenuItem>
+                  <MenuItem value={40}>Querétaro</MenuItem>
+                </Select>
+                <InputLabel id="demo-simple-select-outlined-label">Municipio</InputLabel>
+                <Select
+                  labelId="demo-simple-select-outlined-label"
+                  id="demo-simple-select-outlined"
+                  label='Municipio'
+                  style={{ marginTop: '15px' }}
+                  name='municipio'
+                  required
+                  >
+                  <MenuItem value="">
+                    <em> </em>
+                  </MenuItem>
+                  <MenuItem value={10}>Pachuca</MenuItem>
+                  <MenuItem value={20}>Actopan</MenuItem>
+                  <MenuItem value={30}>San Nicolas</MenuItem>
+                  <MenuItem value={40}>Bernardo Quintana</MenuItem>
+                </Select>
+                <TextField
+                  label='Lugar Especifico'
+                  style={{ marginTop: '15px' }}
+                  name='dependencia'
+                  required
+                />
+                <InputLabel id="demo-simple-select-outlined-label">Responsable de Actividad</InputLabel>
+                <Select
+                  labelId="demo-simple-select-outlined-label"
+                  id="demo-simple-select-outlined"
+                  label='Responsable'
+                  style={{ marginTop: '15px' }}
+                  name='responsable'
+                  required
+                  >
+                  <MenuItem value="">
+                    <em> </em>
+                  </MenuItem>
+                  <MenuItem value={10}>Raúl Arroyo</MenuItem>
+                  <MenuItem value={20}>León Maximiliano Hernández Valdés</MenuItem>
+                  <MenuItem value={30}>Victor Ariel Peréz Benítez</MenuItem>
+                  <MenuItem value={40}>Mercedes Citlali Mendoza Meza</MenuItem>
+                </Select>
+                <InputLabel id="demo-simple-select-outlined-label">Asistente</InputLabel>
+                <Select
+                  labelId="demo-simple-select-outlined-label"
+                  id="demo-simple-select-outlined"
+                  label='Asistente'
+                  style={{ marginTop: '15px' }}
+                  name='asistente'
+                  required
+                  >
+                  <MenuItem value="">
+                    <em> </em>
+                  </MenuItem>
+                  <MenuItem value={10}>Raúl Arroyo</MenuItem>
+                  <MenuItem value={20}>León Maximiliano Hernández Valdés</MenuItem>
+                  <MenuItem value={30}>Victor Ariel Peréz Benítez</MenuItem>
+                  <MenuItem value={40}>Mercedes Citlali Mendoza Meza</MenuItem>
+                </Select>
+                <div className='add-gb'>
+                  <Fab color='primary' aria-label='add' style={{ background: 'green' }} type='submit'>
+                    <DoneIcon />
+                  </Fab>
+                </div>
+              </form>
+            </div>
           </div>
         </div>
-      </div>
-    )
+      )
+    //}
   }
 }
