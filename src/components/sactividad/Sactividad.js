7c44f3da6d88122db8fe4bf3352817305f9e5f10
Feracrz <hitclear23@hotmail.com>
2020-12-16 17:05:21 -0600

    subiendo

--- src/components/sactividad/Sactividad.js
+++ src/components/sactividad/Sactividad.js
@@ -13,6 +13,8 @@ import { withStyles } from '@material-ui/core/styles'
 export default class Sactividad extends Component {
   constructor (props) {
     super(props)
+    this.ref = firebase.firestore().collection('actividades')
+
     this.state = {
       imgeevi: ' ',
       relevancia: '',
@@ -243,10 +245,11 @@ export default class Sactividad extends Component {
       <div className='mg-conta'>
         <div>
           <div className='divtop-mg' />
+
           <div className='form-content-gm'>
-            <form noValidate autoComplete='off' className='mensajesg-container' onSubmit={this.onSubmit}>
+            <form noValidate autoComplete='off' className='mensajesg-container-3' onSubmit={this.onSubmit}>
             <h1>Descripcion de Actividad </h1>
-            
+
             <div className='input-c-c'>
               <p>Convodados:</p>
               <input
@@ -258,86 +261,61 @@ export default class Sactividad extends Component {
               />
               <input className='style-check' type='checkbox' />
             </div>
-            <div className='content-row'>
-              <div className='input-c-c'>
-                <p className='p-t-aa'>Quien Convoca:</p>
-                <select className='select' name='convoca' value={convoca}
-                  onChange={this.onChange}>
-                  <option></option>
-                  <option value="grapefruit">Grapefruit</option>
-                  <option value="lime">Lime</option>
-                  <option selected value="coconut">Coconut</option>
-                  <option value="mango">Mango</option>
-                </select>
+            |
+            <div>
+            <div>
+              <div className='content-row'>
+
+
+                <div className='input-c-c'>
+                    {this.state.actividades.map(actividades =>
+                  <p className='p-t-aa'>{actividades.horai}Quien Convoca:</p>
+                )}
+                </div>
               </div>
               <div className='input-c-c'>
                 <p className='p-t-aa'>Fecha de Inicio:</p>
-                <input type='date' name='fechai' value={fechai}
-                  onChange={this.onChange} />
+
               </div>
             </div>
             <div className='content-row'>
               <div className='input-c-c'>
                 <p className='p-t-aa'>Tipo de Actividad:</p>
-                <select className='select' name='tipoA' value={tipoA}
-                  onChange={this.onChange}>
-                  <option></option>
-                  <option value="grapefruit">Grapefruit</option>
-                  <option value="lime">Lime</option>
-                  <option selected value="coconut">Coconut</option>
-                  <option value="mango">Mango</option>
-                </select>
+
               </div>
               <div className='input-c-c'>
                 <p className='p-t-aa'>Fecha de Fin:</p>
-                <input type='date' name='fechaf' value={fechaf}
-                  onChange={this.onChange} />
+
               </div>
             </div>
             <div className='content-row'>
               <div className='input-c-c'>
                 <p className='p-t-aa'>Estado:</p>
-                <select className='select' name='estado' value={estado}
-                  onChange={this.onChange}>
-                  <option></option>
-                  <option value="grapefruit">Grapefruit</option>
-                  <option value="lime">Lime</option>
-                  <option selected value="coconut">Coconut</option>
-                  <option value="mango">Mango</option>
-                </select>
+
               </div>
               <div className='input-c-c'>
                 <p className='p-t-aa'>Interna Externa:</p>
-                <input className='style-check-i' type='checkbox'
-                  name='internaE' value={internaE}onChange={this.onChange} />
-                <input className='style-check-i' type='checkbox' />
+
               </div>
             </div>
             <div className='content-row'>
               <div className='input-c-c'>
                 <p className='p-t-aa'>Municipio:</p>
-                <select className='select' name='municipio' value={municipio}
-                  onChange={this.onChange}>
-                  <option></option>
-                  <option value="grapefruit">Grapefruit</option>
-                  <option value="lime">Lime</option>
-                  <option selected value="coconut">Coconut</option>
-                  <option value="mango">Mango</option>
-                </select>
+
               </div>
               <div className='input-c-c'>
                 <p className='p-t-aa'>Con quien:</p>
-                <input name='quien' value={quien} onChange={this.onChange} />
+
               </div>
             </div>
             <div className='content-row'>
               <div className='input-c-c'>
                 <p className='p-t-aa'>Lugar:</p>
-                <input name='lugar' value={lugar} onChange={this.onChange} />
+
               </div>
               <div className='input-c-c'>
                 <p className='p-t-aa'>Imparte:</p>
-                <input name='imparte' value={imparte} onChange={this.onChange} />
+
               </div>
             </div>
             <div>
@@ -345,6 +323,9 @@ export default class Sactividad extends Component {
               <textarea cols='80' rows='3' name='desc' value={desc}
                 onChange={this.onChange} />
             </div>
+            </div>
+
+
               <h2>Seguimiento de Actividad</h2>
               <FormControlLabel
                 control={
