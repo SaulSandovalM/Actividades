c4d3ceef42c446a3b36d0e1709c9f5e072790828
Feracrz <hitclear23@hotmail.com>
2021-03-05 13:55:36 -0600

    subindo

--- src/components/Estadisticas/Estadisgeneral.js
+++ src/components/Estadisticas/Estadisgeneral.js
@@ -13,6 +13,14 @@ import { withStyles } from '@material-ui/core/styles'
 import ExportExcel from 'react-export-excel'
 import ReactExport from 'react-export-excel';
 
+import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
 
 
 export  default class  Estadisgeneral extends Component {
@@ -58,6 +66,24 @@ export  default class  Estadisgeneral extends Component {
     this.setState({search: event.target.value});
   }
   render () {
+    const useStyles = makeStyles({
+  table: {
+    minWidth: 650,
+  },
+});
+
+function createData(name, calories, fat, carbs, protein) {
+  return { name, calories, fat, carbs, protein };
+}
+
+const rows = [
+  createData('Total de Actividades', 159, 6.0, 24, 4.0),
+  createData('Actividades Realizadas por Procuraduria', 237, 9.0, 37, 4.3),
+  createData('Actividades organizadas por Otras Instituciones', 262, 16.0, 24, 6.0),
+  createData(' ', 305, 3.7, 67, 4.3),
+  createData('Total', 356, 16.0, 49, 3.9),
+];
+
     const ExcelFile = ExportExcel.ExcelFile;
     const ExcelSheet = ExportExcel.ExcelSheet;
     const ExcelColumn = ExportExcel.ExcelColumn;
@@ -186,14 +212,180 @@ export  default class  Estadisgeneral extends Component {
                     </ExcelFile>
                     </div>
                   </div>
-
-
-
-
-
               </div>
           </form>
         </div>
+
+        <div>
+        <TableContainer component={Paper}>
+    <Table  aria-label="simple table">
+      <TableHead>
+        <TableRow>
+          <TableCell>Actividades Totales</TableCell>
+          <TableCell align="right">Numero </TableCell>
+          <TableCell align="right">%</TableCell>
+          <TableCell align="right"></TableCell>
+          <TableCell align="right"></TableCell>
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        {rows.map((row) => (
+          <TableRow key={row.name}>
+            <TableCell component="th" scope="row">
+              {row.name}
+            </TableCell>
+            <TableCell align="right">45415465</TableCell>
+            <TableCell align="right"></TableCell>
+            <TableCell align="right"></TableCell>
+            <TableCell align="right"></TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  </TableContainer>
+        </div>
+        <div className='tabla-inte'>
+        <TableContainer component={Paper}>
+    <Table  aria-label="simple table">
+      <TableHead>
+        <TableRow>
+          <TableCell>Actividades Generadas por la Procuraduria</TableCell>
+          <TableCell align="right">Numero</TableCell>
+          <TableCell align="right">%</TableCell>
+          <TableCell align="right"></TableCell>
+          <TableCell align="right"></TableCell>
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        {rows.map((row) => (
+          <TableRow key={row.name}>
+            <TableCell component="th" scope="row">
+              {row.name}
+            </TableCell>
+            <TableCell align="right">415</TableCell>
+            <TableCell align="right"></TableCell>
+            <TableCell align="right"></TableCell>
+            <TableCell align="right"></TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  </TableContainer>
+        </div>
+        <div className='tabla-inte'>
+        <TableContainer component={Paper}>
+    <Table  aria-label="simple table">
+      <TableHead>
+        <TableRow>
+          <TableCell>Actividades Organizadas Por Otras Instituciones</TableCell>
+          <TableCell align="right">Numero</TableCell>
+          <TableCell align="right">%</TableCell>
+          <TableCell align="right"></TableCell>
+          <TableCell align="right"></TableCell>
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        {rows.map((row) => (
+          <TableRow key={row.name}>
+            <TableCell component="th" scope="row">
+              {row.name}
+            </TableCell>
+            <TableCell align="right">4465</TableCell>
+            <TableCell align="right">25%</TableCell>
+            <TableCell align="right"></TableCell>
+            <TableCell align="right"></TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  </TableContainer>
+        </div>
+
+        <div className='tabla-inte'>
+        <TableContainer component={Paper}>
+    <Table  aria-label="simple table">
+      <TableHead>
+        <TableRow>
+          <TableCell>Capacitacion </TableCell>
+          <TableCell align="right">Numero</TableCell>
+          <TableCell align="right">%</TableCell>
+          <TableCell align="right"></TableCell>
+          <TableCell align="right"></TableCell>
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        {rows.map((row) => (
+          <TableRow key={row.name}>
+            <TableCell component="th" scope="row">
+              {row.name}
+            </TableCell>
+            <TableCell align="right">454</TableCell>
+            <TableCell align="right">5%</TableCell>
+            <TableCell align="right"></TableCell>
+            <TableCell align="right"></TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  </TableContainer>
+        </div>
+        <div className='tabla-inte'>
+        <TableContainer component={Paper}>
+    <Table  aria-label="simple table">
+      <TableHead>
+        <TableRow>
+          <TableCell>Vinculacion</TableCell>
+          <TableCell align="right">Numero</TableCell>
+          <TableCell align="right">%</TableCell>
+          <TableCell align="right"></TableCell>
+          <TableCell align="right"></TableCell>
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        {rows.map((row) => (
+          <TableRow key={row.name}>
+            <TableCell component="th" scope="row">
+              {row.name}
+            </TableCell>
+            <TableCell align="right">15</TableCell>
+            <TableCell align="right"></TableCell>
+            <TableCell align="right"></TableCell>
+            <TableCell align="right"></TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  </TableContainer>
+        </div>
+        <div className='tabla-inte'>
+        <TableContainer component={Paper}>
+    <Table  aria-label="simple table">
+      <TableHead>
+        <TableRow>
+          <TableCell>Difusion</TableCell>
+          <TableCell align="right">Numero</TableCell>
+          <TableCell align="right">%</TableCell>
+          <TableCell align="right"></TableCell>
+          <TableCell align="right"></TableCell>
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        {rows.map((row) => (
+          <TableRow key={row.name}>
+            <TableCell component="th" scope="row">
+              {row.name}
+            </TableCell>
+            <TableCell align="right">465</TableCell>
+            <TableCell align="right">23%</TableCell>
+            <TableCell align="right"></TableCell>
+            <TableCell align="right"></TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  </TableContainer>
+        </div>
+
       </div>
     )
   }
--- src/components/Estadisticas/Estadisticasint.js
+++ src/components/Estadisticas/Estadisticasint.js
@@ -12,6 +12,14 @@ import {MDCRipple} from '@material/ripple'
 import { withStyles } from '@material-ui/core/styles'
 import ExportExcel from 'react-export-excel'
 
+import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
 
 export  default class  Estadisgeneral extends Component {
   constructor (props) {
@@ -58,15 +66,34 @@ export  default class  Estadisgeneral extends Component {
    })
   }
 
+
   handleChange(event) {
     this.setState({search: event.target.value});
   }
+
   render () {
+    const useStyles = makeStyles({
+  table: {
+    minWidth: 650,
+  },
+});
+
+function createData(name, calories, fat, carbs, protein) {
+  return { name, calories, fat, carbs, protein };
+}
+
+const rows = [
+  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+  createData('Eclair', 262, 16.0, 24, 6.0),
+  createData('Cupcake', 305, 3.7, 67, 4.3),
+  createData('Gingerbread', 356, 16.0, 49, 3.9),
+];
+
+
     const ExcelFile = ExportExcel.ExcelFile;
     const ExcelSheet = ExportExcel.ExcelSheet;
     const ExcelColumn = ExportExcel.ExcelColumn;
-    
-
     const ciudadesMaspobladas = [
       {
         cuidad: "Taller",
@@ -104,7 +131,6 @@ export  default class  Estadisgeneral extends Component {
         pais:45
       },
     ];
-
 
 return (
       <div className='fader-est'>
@@ -296,6 +322,64 @@ return (
               </div>
           </form>
         </div>
+        <div className='tabla-inte'>
+        <TableContainer component={Paper}>
+    <Table  aria-label="simple table">
+      <TableHead>
+        <TableRow>
+          <TableCell>Vinculacion</TableCell>
+          <TableCell align="right">Numero</TableCell>
+          <TableCell align="right">%</TableCell>
+          <TableCell align="right"></TableCell>
+          <TableCell align="right"></TableCell>
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        {rows.map((row) => (
+          <TableRow key={row.name}>
+            <TableCell component="th" scope="row">
+              {row.name}
+            </TableCell>
+            <TableCell align="right">15</TableCell>
+            <TableCell align="right"></TableCell>
+            <TableCell align="right"></TableCell>
+            <TableCell align="right"></TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  </TableContainer>
+        </div>
+        <div className='tabla-inte'>
+        <TableContainer component={Paper}>
+    <Table  aria-label="simple table">
+      <TableHead>
+        <TableRow>
+          <TableCell>Difusion</TableCell>
+          <TableCell align="right">Numero</TableCell>
+          <TableCell align="right">%</TableCell>
+          <TableCell align="right"></TableCell>
+          <TableCell align="right"></TableCell>
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        {rows.map((row) => (
+          <TableRow key={row.name}>
+            <TableCell component="th" scope="row">
+              {row.name}
+            </TableCell>
+            <TableCell align="right">465</TableCell>
+            <TableCell align="right">23%</TableCell>
+            <TableCell align="right"></TableCell>
+            <TableCell align="right"></TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  </TableContainer>
+        </div>
+
+
       </div>
     )
 }
