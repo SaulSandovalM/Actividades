4a0f52992607490ee48afce331de778eb966021f
Ferocrz <hitclear23@gmail.com>
2021-05-10 07:27:54 -0500

    subiendi°

--- src/components/aregistradas/Calendario.js
+++ src/components/aregistradas/Calendario.js
@@ -4,40 +4,34 @@ import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid'
 import { formatDate } from '@fullcalendar/react';
 
-
-
-export default class Calendario extends Component {
-
-
+export default class DemoApp extends React.Component {
 
   render () {
+
+    let str = formatDate(new Date(), {
+      month: 'long',
+      year: 'numeric',
+      day: 'numeric',
+      day: 'numeric',
+      timeZoneName: 'short',
+      timeZone: 'UTC',
+      locale: 'es'
+    });
+
+    console.log(str)
     return (
+      <div className='nav-ma1'>
+        <div className='fader-calendario'>
+          <div className='sub-fader'>
 
-        <div className='nav-ma1'>
-          <div className='fader-calendario'>
-          
-
-
-
-        <div className='sub-fader'>
-
-            <FullCalendar
-                plugins={[ dayGridPlugin ]}
-                initialView="dayGridMonth"
-                weekends={false}
-                events={[
-                      { title: 'event 1', date: '2021-05-06' },
-                      { title: 'event 2', date: '2019-04-02' }
-                      ]}
-             />
-
-
-
-
-
+      <FullCalendar
+        plugins={[ dayGridPlugin ]}
+        initialView="dayGridMonth"
+      />
           </div>
-          </div>
+        </div>
       </div>
+
 
     )
   }
