b46baccf6c5f5c546f1e3feefc780247337e2974
Feracruz <hitclear23@gmail.com>
2021-05-11 09:07:53 -0500

    subiendo

--- src/components/aregistradas/Calendario.js
+++ src/components/aregistradas/Calendario.js
@@ -2,32 +2,41 @@ import React, { Component } from 'react'
 import './Aregistradas.css'
 import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid'
-import { formatDate } from '@fullcalendar/react';
+import { formatDate } from '@fullcalendar/react'
+import { Calendar } from '@fullcalendar/core';
+import listPlugin from '@fullcalendar/list';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import resourceTimelinePlugin from '@fullcalendar/resource-timeline';
+
 
 export default class DemoApp extends React.Component {
 
   render () {
 
-    let str = formatDate(new Date(), {
-      month: 'long',
-      year: 'numeric',
-      day: 'numeric',
-      day: 'numeric',
-      timeZoneName: 'short',
-      timeZone: 'UTC',
-      locale: 'es'
-    });
+      let str = formatDate(new Date(), {
+        month: 'short',
+        year: 'numeric',
+        day: 'numeric',
+        day: 'numeric',
+        timeZoneName: 'short',
+        timeZone: 'UTC',
+        locale: 'Es',
+        omitCommas: true,
+        weekday: 'short',
+      });
 
-    console.log(str)
+
+
+
+
     return (
       <div className='nav-ma1'>
         <div className='fader-calendario'>
-          <div className='sub-fader'>
-
-      <FullCalendar
-        plugins={[ dayGridPlugin ]}
-        initialView="dayGridMonth"
-      />
+          <div className='sub-fader fc fc-col-header-cell-cushion'>
+            <FullCalendar
+              plugins={[ dayGridPlugin ]}
+              initialView="dayGridMonth"
+            />
           </div>
         </div>
       </div>
