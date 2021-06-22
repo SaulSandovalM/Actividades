60400859e3284036e15073ede1e1fc5613537c2a
Feracruz <hitclear23@gmail.com>
2021-06-22 12:19:14 -0500

    subiendo2

--- src/components/documentos/Reporteniveldir.js
+++ src/components/documentos/Reporteniveldir.js
@@ -23,7 +23,7 @@ export default class Reporteniveldir extends Component {
     var numfila = 0
     querySnapshot.forEach((doc) => {
       numfila++
-      const { tipoActividad, imparte, lugar, fechai, fechaf, convoca, dependencia, horai, objetivo} = doc.data()
+      const { tipoActividad, imparte, actividad, lugar, fechai, fechaf, convoca, dependencia, horai, objetivo} = doc.data()
       actividades.push({
         key: doc.id,
         doc,
@@ -37,6 +37,7 @@ export default class Reporteniveldir extends Component {
         horai,
         objetivo,
         lugar,
+        actividad,
       })
     })
     this.setState({
@@ -54,19 +55,68 @@ export default class Reporteniveldir extends Component {
   render () {
     return (
       <div className='fader-pfd'>
+      <div className='btn-imprimir'>
+        <span class="material-icons" style={{ cursor:'pointer'  }} onClick={this.handleBack.bind(this)}>
+        </span>
+      <div clasName=''>
+        <ReactToPrint
+          trigger={() =>
+        <span class='material-icons impresora-padding' style={{ cursor:'pointer' }}>
+          <Fab color='primary' aria-label='add' style={{ background: '#092432' }} type='submit'>
+            <ImpIcon />
+          </Fab>
+        </span>}
+          content={() => this.agenda}
+        />
+        <div className="regresars">
+        <span class="material-icons" style={{ cursor:'pointer'  }} onClick={this.handleBack.bind(this)}>
+          <Fab color='primary' aria-label='add' style={{ background: '#092432' }} type='submit'>
+            <ReplyIcon />
+          </Fab>
+        </span>
+        </div>
+      </div>
+      </div>
 
-        <div>
+        <div className='documento'  ref={el => (this.agenda = el)}>
+        {/*{this.state.actividades.map(actividades =>*/}
           <div clasName='titulos-pdf'>
-            <div className='cargos textos-ca'>
+            <div className='cargos '>
               <p className='textos-ca name1'>Nombre:</p>
-              <h5 className='txtc'>{/*{actividades.convoca}*/}DIANA CORONA MENESES </h5>
+              <h5 className='textc'>{/*{actividades.convoca}*/}DIANA CORONA MENESES </h5>
             </div>
-            <div className='cargos textos-ca'>
+
+            <div className='cargos '>
               <p className='textos-ca name2'>Cargo:</p>
               <h5 className='textc'>{/*{actividades.dependencia}*/}SECRETARIA PARTICULAR DEL DESPACHO DEL PROCURADOR</h5>
             </div>
-          </div>
-
+            </div>
+            <div className='tabla-reporte'>
+              <table>
+                <tr>
+                  <th className='all-tabla tabla-n'>No.</th>
+                  <th className='all-tabla tabla-top color-t'>Fecha</th>
+                  <th className='all-tabla tabla-top color-t2'>Hora</th>
+                  <th className='all-tabla tabla-top color-t'>Lugar</th>
+                  <th className='all-tabla tabla-top color-t2'>Actividad</th>
+                  <th className='all-tabla tabla-top color-t'>Beneficio para la PGJEH</th>
+                  <th className='all-tabla tabla-top color-t2'>Relevancia</th>
+                </tr>
+                {
+                  this.state.actividades.map(actividades =>
+                <tr>
+                  <td className='all-tabla color-tn'>{actividades.numfila}</td>
+                  <td className='all-tabla tabla-f'>{actividades.fechai }</td>
+                  <td className='all-tabla tabla-h'>{actividades.horai } hrs.</td>
+                  <td className='all-tabla tabla-l'>{actividades.lugar}</td>
+                  <td className='all-tabla tabla-a'>{actividades.tipoActividad}: {actividades.actividad}</td>
+                  <td className='all-tabla tabla-b'>{actividades.evidencia}Texto de prueba</td>
+                  <td className='all-tabla tabla-r'>{actividades.relevacia}Relevancia justificada por el sistema (prueba)</td>
+                </tr>
+                )
+              }
+              </table>
+            </div>
         </div>
 
 
@@ -79,96 +129,20 @@ export default class Reporteniveldir extends Component {
 
 
 
-          <div className='fader-reporte-r' >
-        <div>
-        <div ref={el => (this.agenda = el)}>
-          <div className='all'>
-          <div className='Nombre-cargo' >
-            <div>
-              <div className='namex1'>
-                <p className='name'>Nombre:</p>
-              </div>
-              <div className=''>
-                <p className='cargo'>Cargo:</p>
-              </div>
-            </div>
-            {/*{this.state.actividades.map(actividades =>*/}
-            <div className='Nom-car'>
-              <h3 className='h3-top'>{/*{actividades.convoca}*/}DIANA CORONA MENESES </h3>
-              <h3 className='h3-top'>{/*{actividades.dependencia}*/}SECRETARIA PARTICULAR DEL DESPACHO DEL PROCURADOR
-              </h3>
-            </div>
-          </div>
-
-
-
-          <div className="tabla-dir">
-          <table>
-            <tr>
-            <th className='all-tabla tabla-n'>No.</th>
-              <th className='all-tabla tabla-top color-t'>Fecha</th>
-              <th className='all-tabla tabla-top color-t2'>Hora</th>
-              <th className='all-tabla tabla-top color-t'>Lugar</th>
-              <th className='all-tabla tabla-top color-t2'>Actividad</th>
-              <th className='all-tabla tabla-top color-t'>Beneficio para la PGJEH</th>
-              <th className='all-tabla tabla-top color-t2'>Relevancia</th>
-            </tr>
-              {this.state.actividades.map(actividades =>
-            <tr>
-              <td className='all-tabla color-t'>{actividades.numfila}</td>
-              <td className='all-tabla tabla-f'>{actividades.fechai }</td>
-              <td className='all-tabla tabla-h'>{actividades.horai } hrs.</td>
-              <td className='all-tabla tabla-l'>{actividades.lugar}</td>
-              <td className='all-tabla tabla-a'>{actividades.tipoActividad }</td>
-              <td className='all-tabla tabla-b'>{actividades.evidencia}Texto de prueba</td>
-              <td className='all-tabla tabla-r'>{actividades.relevacia}Relevancia justificada por el sistema (prueba)</td>
-            </tr>
-
-
-
-          )}
 
 
 
 
 
-          </table>
-          </div>
 
 
 
-            </div>
-
-          </div>
 
 
-          </div>
 
-        </div>
-          <div className='btn-imprimir'>
-            <span class="material-icons" style={{ cursor:'pointer'  }} onClick={this.handleBack.bind(this)}>
-              <Fab color='primary' aria-label='add' style={{ background: '#092432' }} type='submit'>
-                <ReplyIcon />
-              </Fab>
-                {/*<p className='txt-impri'>
-                  Regresar
-                </p>*/}
-            </span>
-            <div clasName=''>
-              <ReactToPrint
-                trigger={() =>
-              <span class='material-icons impresora-padding' style={{ cursor:'pointer' }}>
-                  <Fab color='primary' aria-label='add' style={{ background: '#092432' }} type='submit'>
-                    <ImpIcon />
-                  </Fab>
-              </span>}
-                content={() => this.agenda}
-              />
-                {/*<p className='txt-impri'>
-                  Imprimir
-                </p>*/}
-            </div>
-            </div>
+
+
+
       </div>
 
     )
