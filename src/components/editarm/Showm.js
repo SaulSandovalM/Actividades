75c9409ea520aea917a709896be66d2c10806e2b
FeraCrz <ferota1234gmail.com>
2021-07-14 17:18:08 -0500

    subiendo

--- src/components/editarm/Showm.js
+++ src/components/editarm/Showm.js
@@ -5,7 +5,6 @@ import { Link } from 'react-router-dom'
 import Fab from '@material-ui/core/Fab';
 import AddIcon from '@material-ui/icons/Add'
 import { AccessAlarm, ThreeDRotation } from '@material-ui/icons';
-import { TableContainer, Table, TableHead, TableBody, TableRow, TableCell } from '@material-ui/core';
 import MaterialTable from 'material-table';
 
 
@@ -21,6 +20,12 @@ export default class Showm extends Component {
       messages: []
     }
   }
+  handleBack() {
+      this.props.history.push('/Editarmensaje/${messages.key}');
+    }
+    handleBack() {
+        this.props.history.push('//Generaciondemensajes');
+      }
 
   onCollectionUpdate = (querySnapshot) => {
     const messages = []
@@ -63,76 +68,23 @@ export default class Showm extends Component {
           {
             title: 'FECHA',
             field: 'fecha'
-          },
-          {
-            title: 'OPCIONES',
-            field: 'buttom'
           }
 
         ];
-
-
-
-
 
         const tabla = this.state.messages.map(messages => messages)
 
         console.log (tabla)
 
     return (
-      <div className='mg-conta'>
-        <div className='nav-mm'>
-          <h1 className='h1-lm'>Agregar, Editar o eMensajes </h1>
+      <div className='mg-conta-msj'>
+        <div className='nav-mm-msj'>
+          <h1 className='h1-lms'>Agregar, Editar o Mensajes </h1>
           </div>
-        <div className='mes-center' style={{ position: 'fixed', marginTop: '120px', background: '#fafafa' }}>
-          <div className='mes-container' style={{ marginRight: '256px' }}>
-          <div className='head-mes' style={{paddingLeft: '6.5%', color: 'grey'}}>
-              Asunto
-            </div>
-            <div className='head-mesd' style={{ color: 'grey' }}>
-              Descripcion
-            </div>
-            <div className='head-mesf' style={{ color: 'grey' }}>
-              Fecha
-            </div>
-            <div className='one-po' />
-          </div>
-        </div>
 
-        <div style={{paddingTop: '195px'}}>
-          {this.state.messages.map(messages =>
-            <div className='mes-center2'>
-              <div className='mes-container-map'>
-                <div className='head-mes' style={{fontWeight: 'bold'}}>
-                   ✮ {messages.asunto}
-                </div>
-                <div className='head-mesd' style={{color: '#424242'}}>
-                  {messages.descripcion}
-                </div>
-                <div className='head-mesf' style={{color: '#424242'}}>
-                  {messages.fecha}
-                </div>
-                <div className='one-po icons-delete'>
-                  <Link to={`/Editarmensaje/${messages.key}`}>
-                    <span className='material-icons icon-edit'>
-                      create
-                    </span>
-                    </Link>
-                    <Link>
-                      <span class="material-icons" >
-                      delete
-                      </span>
-                    </Link>
-                    <div>
-                    <button > Eliminar</button>
-                    </div>
 
-                  </div>
+        <div style={{paddingTop: '0%'}}>
 
-              </div>
-            </div>
-
-          )}
         </div>
 
         <div className='add-m' style={{position: 'fixed'}}>
@@ -144,47 +96,34 @@ export default class Showm extends Component {
         </div>
         <div className='dad-tablita'>
         <div className='tablita'>
-        <TableContainer>
-          <Table>
-            <TableHead>
-              <TableRow className='conte-tablita'>
-                <TableCell style={{ color: '#dc5f70', fontWeight: 'bold' }}>ASUNTO</TableCell>
-                <TableCell style={{ color: '#dc5f70', fontWeight: 'bold'}} >DESCRIPCION</TableCell>
-                <TableCell style={{ color: '#dc5f70', fontWeight: 'bold'}} >FECHA</TableCell>
-                <TableCell style={{ color: '#dc5f70', fontWeight: 'bold'}} >OPCIONES</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              <TableRow>
-                <TableCell>Adios</TableCell>
-                <TableCell>Adios</TableCell>
-                <TableCell>Adios</TableCell>
-                <TableCell>Adios</TableCell>
 
-              </TableRow>
-            </TableBody>
-          </Table>
-        </TableContainer>
         </div>
         </div>
 
 
-        <div>
+        <div className='tabla-msjx'>
                   <MaterialTable
                     columns={columnas}
                     data = {tabla}
                     title = "Mensajes"
+                    actions = "Opciones"
 
                     actions = {[
                       {
+                        icon: 'add',
+                        tooltip: 'Agregar Nuevo Mensaje',
+                        isFreeAction: true,
+                        onClick: (this.handleBack.bind(this))
+                      },
+                      {
                         icon: 'edit',
                         tooltip: 'Editar',
-                        onClick: (event, rowData)=>Link('/Editarmensaje/${messages.key}')
+                        onClick: (this.handleBack.bind(this))
                       },
                       {
                         icon: 'delete',
                         tooltip: 'Editar artista',
-                        onClick: (event, rowData)=>window.confirm('Estas seguro que quires elimar el mensaje : ' +rowData.artista+'?')
+                        onClick: (event, rowData)=>window.confirm('¿Deseas Eliminar el Mensaje?' +rowData.artista)
                       }
                     ] }
 
@@ -300,7 +239,7 @@ export default class Showm extends Component {
 //
 //   return (
 //     <div className= 'mg-conta'>
-//       <div className = 'nav-mm'>
+//       <div className = ''>
 //         <h1 className = 'h1-lm'>
 //           <span class = "material-icons msj-d"> speaker_notes < /span>
 //           Vista de Pruebas
