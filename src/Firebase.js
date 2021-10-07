9abfa164809f52d4e4201d58033d0a2adf971dda
FeraCrz <ferota1234gmail.com>
2021-10-07 09:43:52 -0500

    subiendo

--- src/Firebase.js
+++ src/Firebase.js
@@ -5,23 +5,23 @@ import 'firebase/database'
 import 'firebase/storage'
 
 const config = {
-  // apiKey: 'AIzaSyCzLnwq0jQ_WbsviOrtu12TZwgDrigrHI0',
-  // authDomain: 'archivos-b0b9f.firebaseapp.com',
-  // databaseURL: 'https://archivos-b0b9f.firebaseio.com',
-  // projectId: 'archivos-b0b9f',
-  // storageBucket: 'archivos-b0b9f.appspot.com',
-  // messagingSenderId: '199484465454',
-  // appId: '1:199484465454:web:ea89ad7c7af536c5fb9abf',
-  // measurementId: 'G-4WNHLET6KE'
+  apiKey: 'AIzaSyCzLnwq0jQ_WbsviOrtu12TZwgDrigrHI0',
+  authDomain: 'archivos-b0b9f.firebaseapp.com',
+  databaseURL: 'https://archivos-b0b9f.firebaseio.com',
+  projectId: 'archivos-b0b9f',
+  storageBucket: 'archivos-b0b9f.appspot.com',
+  messagingSenderId: '199484465454',
+  appId: '1:199484465454:web:ea89ad7c7af536c5fb9abf',
+  measurementId: 'G-4WNHLET6KE'
 
 
-   apiKey: "AIzaSyBcnCsZlT4TR8RYm21KM9t8TmEmN17S3jI",
-   authDomain: "actividades-a2936.firebaseapp.com",
-   projectId: "actividades-a2936",
-   storageBucket: "actividades-a2936.appspot.com",
-   messagingSenderId: "446841894263",
-   appId: "1:446841894263:web:696f4c23335f294ecb9d72",
-   measurementId: "G-3EQM3CQ5E6"
+   // apiKey: "AIzaSyBcnCsZlT4TR8RYm21KM9t8TmEmN17S3jI",
+   // authDomain: "actividades-a2936.firebaseapp.com",
+   // projectId: "actividades-a2936",
+   // storageBucket: "actividades-a2936.appspot.com",
+   // messagingSenderId: "446841894263",
+   // appId: "1:446841894263:web:696f4c23335f294ecb9d72",
+   // measurementId: "G-3EQM3CQ5E6"
 }
 firebase.initializeApp(config)
 firebase.firestore()
