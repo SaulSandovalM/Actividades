304bd7c40729656ae3a8214b5ec0a78de6f2b107
FeraCrz <ferota1234gmail.com>
2021-10-11 15:52:39 -0500

    subiendo

--- src/components/aregistradas/Agenda.js
+++ src/components/aregistradas/Agenda.js
@@ -4,6 +4,7 @@ import './Aregistradas.css'
 import TextField from '@material-ui/core/TextField'
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { ViewState, EditingState } from '@devexpress/dx-react-scheduler';
+import firebase from '../../Firebase'
 import {
   Scheduler,
   Resources,
@@ -17,6 +18,8 @@ import {
   DragDropProvider,
   Toolbar,
 } from '@devexpress/dx-react-scheduler-material-ui';
+
+import { green, deepOrange, lightBlue } from '@material-ui/core/colors';
 import { owners } from './tasks';
 import { appointments, resourcesData } from './resources';
 //botones
@@ -24,43 +27,77 @@ import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 
 export default class Demo extends React.PureComponent {
-  constructor(props) {
-    super(props);
+  constructor (props) {
+    super(props)
+    this.ref = firebase.firestore().collection('actividades').orderBy('horai', 'asc')
+    this.unsubscribe = null
     this.state = {
-      data: appointments,
-      resources: [
-        {
-          fieldName: 'roomId',
-          title: 'Room',
-          instances: resourcesData,
-        },
-        {
-          fieldName: 'members',
-          title: 'Members',
-          instances: owners,
-          allowMultiple: true,
-        },
-      ],
-    };
+      actividades: [],
+      abierto: false,
+      fechai: '',
+      tipoActividad: '',
 
-    this.commitChanges = this.commitChanges.bind(this);
+    }
   }
+
+
+  onCollectionUpdate = (querySnapshot) => {
+    const actividades = []
+    querySnapshot.forEach((doc) => {
+      const { tipoActividad, fechai  } = doc.data()
+      actividades.push({
+        key: doc.id,
+        doc,
+        tipoActividad,
+        fechai,
+      })
+    })
+    this.setState({
+      actividades
+   })
+  }
+  componentDidMount () {
+    this.unsubscribe = this.ref.onSnapshot(this.onCollectionUpdate)
+
+  }
+
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     data: appointments,
+  //     resources: [
+  //       {
+  //         fieldName: 'roomId',
+  //         title: 'Room',
+  //         instances: resourcesData,
+  //       },
+  //       {
+  //         fieldName: 'members',
+  //         title: 'Members',
+  //         instances: owners,
+  //         allowMultiple: true,
+  //       },
+  //     ],
+  //   };
+  //
+  //   this.commitChanges = this.commitChanges.bind(this);
+  // }
 
   commitChanges({ added, changed, deleted }) {
     this.setState((state) => {
-      let { data } = state;
+      let { actividades } = state;
       if (added) {
-        const startingAddedId = data.length > 0 ? data[data.length - 1].id + 1 : 0;
-        data = [...data, { id: startingAddedId, ...added }];
+        const startingAddedId = actividades.length > 0 ? actividades[actividades.length - 1].id + 1 : 0;
+        actividades = [...actividades, { id: startingAddedId, ...added }];
       }
       if (changed) {
-        data = data.map(appointment => (
-          changed[appointment.id] ? { ...appointment, ...changed[appointment.id] } : appointment));
+        actividades = actividades.map(appointment => (
+          changed[appointment.id] ?  { ...appointment, ...changed[appointment.id] } : appointment));
       }
       if (deleted !== undefined) {
-        data = data.filter(appointment => appointment.id !== deleted);
+        actividades = actividades.filter(appointment => appointment.id !== deleted);
       }
-      return { data };
+      return { actividades };
     });
   }
 
@@ -68,7 +105,43 @@ export default class Demo extends React.PureComponent {
     this.props.history.push('/AgregarActividad');
   }
   render() {
+    var array = [{title: '', date: ''}]
+
     const { data, resources } = this.state;
+
+    var array = [{title: '', date: ''}]
+    this.state.actividades.map(item => {
+    array.push({title: item.tipoActividad, date: item.fechai})
+    })
+    console.log(array)
+
+  const owners = [
+      {
+        text: 'Andrew Glover',
+        id: 1,
+        color: '#7E57C2',
+      }, {
+        text: 'Arnie Schwartz',
+        id: 2,
+        color: '#FF7043',
+      }, {
+        text: 'John Heart',
+        id: 3,
+        color: '#E91E63',
+      }, {
+        text: 'Taylor Riley',
+        id: 4,
+        color: '#E91E63',
+      }, {
+        text: 'Brad Farkus',
+        id: 5,
+        color: '#AB47BC',
+      }, {
+        text: 'Arthur Miller',
+        id: 6,
+        color: '#FFA726',
+      },
+    ];
 
     return (
       <div className= 'agenda-fader'>
@@ -85,7 +158,7 @@ export default class Demo extends React.PureComponent {
         <div>
       <Paper>
         <Scheduler
-          data={data}
+          data={array}
           locale="Es"
           >
         <ViewState
